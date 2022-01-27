07c4be09a6162a0f4908ba5135eb4b9002e48ced
neripark <neripark@gmail.com>
2022-01-27 16:13:46 +0900

    install serverless-offline

--- serverless.ts
+++ serverless.ts
@@ -5,7 +5,7 @@ import hello from '@functions/hello';
 const serverlessConfiguration: AWS = {
   service: 'serverless-framework-playground',
   frameworkVersion: '2',
-  plugins: ['serverless-esbuild'],
+  plugins: ['serverless-esbuild', 'serverless-offline'],
   provider: {
     name: 'aws',
     runtime: 'nodejs14.x',
