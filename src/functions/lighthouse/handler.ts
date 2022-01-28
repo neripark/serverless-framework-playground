22c5f15693e13403774358ac7775aadb9c45b2b8
neripark <neripark@gmail.com>
2022-01-28 14:19:53 +0900

    tmp

--- src/functions/lighthouse/handler.ts
+++ src/functions/lighthouse/handler.ts
@@ -1,10 +1,14 @@
 import type { ValidatedEventAPIGatewayProxyEvent } from '@libs/apiGateway';
 import { formatJSONResponse } from '@libs/apiGateway';
 import { middyfy } from '@libs/lambda';
+import { execLighthouse } from './execLighthouse';
 
 import schema from './schema';
 
 const lighthouse: ValidatedEventAPIGatewayProxyEvent<typeof schema> = async (event) => {
+  const str = execLighthouse();
+  console.log(str);
+  
   return formatJSONResponse({
     message: `You got lighthouse score.`,
     event,
